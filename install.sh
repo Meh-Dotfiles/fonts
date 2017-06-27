1478ef2fe8f85fe616e5e53fc205174023bda9e9
Sergi <Sergi030@users.noreply.github.com>
2017-06-27 16:44:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 mkdir /tmp/.meh-fonts
 env git clone https://github.com/Meh-Dotfiles/fonts.git /tmp/.meh-fonts
 cd /tmp/.meh-fonts
-./font-install.sh
+./install-fonts.sh
 cd 
 rm -rf /tmp/.meh-fonts
